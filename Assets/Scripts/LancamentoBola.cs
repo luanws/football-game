e8ffe7d0baf4e4f958384263f77f3ff28a4d44d3
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-05-19 23:54:22 -0300

    Update LancamentoBola.cs

--- Assets/Scripts/LancamentoBola.cs
+++ Assets/Scripts/LancamentoBola.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class LancamentoBola : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rigidbody2D;
+    public float forca = 1000;
+    private RotacaoSeta rotacaoSeta;
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rotacaoSeta = GetComponent<RotacaoSeta>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            rigidbody2D.AddForce(polar(forca, rotacaoSeta.rotacao));
+        }
+    }
+
+    private Vector2 polar(float raio, float angulo)
+    {
+        float x = raio * Mathf.Cos(angulo * Mathf.Deg2Rad);
+        float y = raio * Mathf.Sin(angulo * Mathf.Deg2Rad);
+        return new Vector2(x, y);
     }
 }
