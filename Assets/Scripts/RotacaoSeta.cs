22860e76f58539fba6727ceda41ce069eb44059d
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-05-19 23:36:39 -0300

    Rotação

---
+++ Assets/Scripts/RotacaoSeta.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotacaoSeta : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Transform posicaoInicial;
+    [SerializeField] private Image setaImage;
+    public float rotacao;
+
+    private void Start()
+    {
+        posicionarBola();
+        posicionarSeta();
+    }
+
+    private void Update()
+    {
+        rotacionarSeta();
+        inputRotacao();
+    }
+
+    private void posicionarBola()
+    {
+        gameObject.transform.position = posicaoInicial.position;
+    }
+
+    private void posicionarSeta()
+    {
+        setaImage.rectTransform.position = posicaoInicial.position;
+    }
+
+    private void rotacionarSeta()
+    {
+        setaImage.rectTransform.eulerAngles = new Vector3(0, 0, rotacao);
+    }
+
+    private void inputRotacao()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rotacao += 2.5f;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rotacao -= 2.5f;
+        }
+    }
+}
+
