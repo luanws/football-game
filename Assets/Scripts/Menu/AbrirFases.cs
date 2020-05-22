8e03e3f3e42c34490ed386eceb6c6af42a088e2f
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-05-22 17:28:21 -0300

    Abrir fases ao clicar em jogar

---
+++ Assets/Scripts/Menu/AbrirFases.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AbrirFases : MonoBehaviour {
+    public Button button;
+    void Start() {
+        button.onClick.AddListener(() => {
+            SceneManager.LoadScene("Scenes/Fases");
+        });
+    }
+}
