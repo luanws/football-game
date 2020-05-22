6ebefd7ccdb35b12f1f3d658a9f28d124822e289
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-05-22 16:12:26 -0300

    Gerenciador de pontuação

---
+++ Assets/Scripts/Fases/GerenciadorPontuacao.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerenciadorPontuacao : MonoBehaviour
+{
+    public static GerenciadorPontuacao instancia;
+    public int moedas {
+        get {
+            return PlayerPrefs.GetInt("moedas", 100);
+        }
+        set {
+            PlayerPrefs.SetInt("moedas", value);
+        }
+    }
+
+    void Awake() {
+        if(instancia == null) {
+            instancia = this;
+            DontDestroyOnLoad(this.gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    public void adicionarMoedas(int novasMoedas) {
+        moedas += novasMoedas;
+    }
+
+    public void removerMoedas(int perdaMoedas) {
+        moedas -= perdaMoedas;
+    }
+}
