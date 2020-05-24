7a29a9731765391321b4a5c6406a7c1efa2da9f6
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-05-23 22:07:39 -0300

    espeto prefab

---
+++ Assets/Scripts/Inimigos/Espeto/Oscilar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscilar : MonoBehaviour
+{
+    private SliderJoint2D slider;
+    private JointMotor2D motor;
+    private float min;
+    private float max;
+
+    void Start() {
+        slider = GetComponent<SliderJoint2D>();
+        motor = slider.motor;
+        min = slider.limits.min;
+        min = slider.limits.min;
+    }
+
+    void Update() {
+        if (slider.limitState == JointLimitState2D.UpperLimit) {
+            motor.motorSpeed = -Mathf.Abs(motor.motorSpeed);
+            slider.motor = motor;
+        } else if (slider.limitState == JointLimitState2D.LowerLimit) {
+            motor.motorSpeed = Mathf.Abs(motor.motorSpeed);
+            slider.motor = motor;
+        }
+    }
+}
