f8b7af14e765f728cdbfae7259f19f1e13f0bfa4
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-05-24 15:54:00 -0300

    Exibe quantidade de moedas

---
+++ Assets/Scripts/Coletaveis/ExibirPontuacao.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExibirPontuacao : MonoBehaviour {
+    private Text text;
+    private void Start() {
+        text = GetComponent<Text>();
+    }
+    private void Update() {
+        text.text = Pontuacao.instance.moedas.ToString();
+    }
+}
