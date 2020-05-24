c77ddaa39658f149908d266c548852bbbb98a0a4
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-05-24 20:09:51 -0300

    KinematicControl

---
+++ Assets/Scripts/Bola/KinematicControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KinematicControl : MonoBehaviour {
+    private Rigidbody2D rigidbody2D;
+    
+    private void Start() {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    public void Kinematic() {
+        rigidbody2D.isKinematic = true;
+    }
+
+    public void RemoveKinematic() {
+        rigidbody2D.isKinematic = false;
+    }
+}
