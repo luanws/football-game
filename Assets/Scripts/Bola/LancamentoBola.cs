651448824aa09c831d59910fafc64772c932bb95
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-05-23 22:50:06 -0300

    Update LancamentoBola.cs

--- Assets/Scripts/Bola/LancamentoBola.cs
+++ Assets/Scripts/Bola/LancamentoBola.cs
@@ -3,56 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LancamentoBola : MonoBehaviour
-{
+public class LancamentoBola : MonoBehaviour {
     private Rigidbody2D rigidbody2D;
     public float forca = 1000;
     private RotacaoSeta rotacaoSeta;
     [SerializeField] private Image setaVerde;
-    void Start()
-    {
+    void Start() {
         rigidbody2D = GetComponent<Rigidbody2D>();
         rotacaoSeta = GetComponent<RotacaoSeta>();
     }
 
-    void Update()
-    {
-        if (rotacaoSeta.chuteLiberado)
-        {
+    void Update() {
+        if (rotacaoSeta.chuteLiberado) {
             chutar();
         }
         controlarForca();
     }
 
-    private Vector2 polar(float raio, float angulo)
-    {
+    private Vector2 polar(float raio, float angulo) {
         float x = raio * Mathf.Cos(angulo * Mathf.Deg2Rad);
         float y = raio * Mathf.Sin(angulo * Mathf.Deg2Rad);
         return new Vector2(x, y);
     }
 
-    private void chutar()
-    {
+    private void chutar() {
         rigidbody2D.AddForce(polar(forca, rotacaoSeta.rotacao));
         rotacaoSeta.chuteLiberado = false;
     }
 
-    private void controlarForca()
-    {
-        if (rotacaoSeta.rotacaoLiberada)
-        {
+    private void controlarForca() {
+        if (rotacaoSeta.rotacaoLiberada) {
             Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             float distancia = Vector2.Distance(mouse, transform.position);
-            if (distancia > 0)
-            {
+            if (distancia > 0) {
                 setaVerde.fillAmount = distancia / 2;
                 forca = setaVerde.fillAmount * 1000;
             }
         }
     }
 
-    private void OnMouseDown()
-    {
+    private void OnMouseDown() {
         rigidbody2D.Sleep();
     }
 }
