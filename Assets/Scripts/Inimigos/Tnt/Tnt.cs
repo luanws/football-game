f555622ea2499615c7ef00d29f28c841071437f5
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-05-23 23:32:07 -0300

    Tnt

---
+++ Assets/Scripts/Inimigos/Tnt/Tnt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tnt : MonoBehaviour {
+    [SerializeField] 
+    private GameObject explosaoPrefab;
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            Instantiate(explosaoPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
