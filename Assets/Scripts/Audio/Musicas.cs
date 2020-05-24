c97c395ec2cba9678b94fbbc9dc4a0990cf9a4b7
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-05-24 16:26:59 -0300

    Musicas

---
+++ Assets/Scripts/Audio/Musicas.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Musicas : MonoBehaviour {
+    public AudioClip[] audioClips;
+    public AudioSource audioSource;
+    public static Musicas instance;
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update() {
+        if (!audioSource.isPlaying) {
+            audioSource.clip = GetRandomAudioClip();
+            audioSource.Play();
+        }
+    }
+
+    private AudioClip GetRandomAudioClip() {
+        return audioClips[Random.Range(0, audioClips.Length)];
+    }
+}
