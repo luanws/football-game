0ebfc6c49217109a223a1a69d2b44620216c9ff7
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-05-25 19:24:53 -0300

    FollowBall

---
+++ Assets/Scripts/Camera/FollowBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowBall : MonoBehaviour {
+    private GameObject ball;
+    private GameManager gameManager;
+
+    private void Start() {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        
+    }
+
+    private void Update() {
+        ball = GameObject.Find("Ball");
+        Vector3 position = transform.position;
+        position.x = ball.transform.position.x;
+        position.x = Mathf.Clamp(position.x, gameManager.limitLeft, gameManager.limitRight);
+        transform.position = position;
+    }
+}
