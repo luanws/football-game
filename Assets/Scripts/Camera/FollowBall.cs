8bc1bdaa8dfa9a0b3958532b1ef1ec9be9b2bb91
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-05-25 19:41:29 -0300

    Update FollowBall.cs

--- Assets/Scripts/Camera/FollowBall.cs
+++ Assets/Scripts/Camera/FollowBall.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class FollowBall : MonoBehaviour {
     private GameObject ball;
     private GameManager gameManager;
+    private float limitLeft, limitRight;
 
     private void Start() {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        
+
+        float screenHeight = Camera.main.orthographicSize * 2;
+        float screenWidth = screenHeight / Screen.height * Screen.width;
+        limitLeft = gameManager.limitLeft + (screenWidth / 2);
+        limitRight = gameManager.limitRight - (screenWidth / 2);
     }
 
     private void Update() {
@@ -16,7 +21,7 @@ public class FollowBall : MonoBehaviour {
         if (ball != null) {
             Vector3 position = transform.position;
             position.x = ball.transform.position.x;
-            position.x = Mathf.Clamp(position.x, gameManager.limitLeft, gameManager.limitRight);
+            position.x = Mathf.Clamp(position.x, limitLeft, limitRight);
             transform.position = position;
         }
     }
