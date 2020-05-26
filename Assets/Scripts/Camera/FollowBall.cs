688cecb658c01755591b0911cc5d1ecf82bb327b
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-05-25 21:31:43 -0300

    câmera volta em movimento suave

--- Assets/Scripts/Camera/FollowBall.cs
+++ Assets/Scripts/Camera/FollowBall.cs
@@ -6,6 +6,8 @@ public class FollowBall : MonoBehaviour {
     private GameObject ball;
     private GameManager gameManager;
     private float limitLeft, limitRight;
+    private float t = 1;
+    [SerializeField] private float velocitySmoothStepCamera = 0.5f;
 
     private void Start() {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
@@ -20,7 +22,15 @@ public class FollowBall : MonoBehaviour {
         ball = GameObject.Find("Ball");
         if (ball != null) {
             Vector3 position = transform.position;
-            position.x = ball.transform.position.x;
+            
+            if (transform.position.x != limitLeft && ball.GetComponent<Rigidbody2D>().velocity == Vector2.zero) {
+                t -= velocitySmoothStepCamera * Time.deltaTime;
+                position.x = Mathf.SmoothStep(limitLeft, position.x, t);
+            } else {
+                t = 1;
+                position.x = ball.transform.position.x;
+            }
+
             position.x = Mathf.Clamp(position.x, limitLeft, limitRight);
             transform.position = position;
         }
